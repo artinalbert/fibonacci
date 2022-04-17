017f865876b26fbf6fc409f37c574fd3bd6e4063
Amirhossein Dabaghmanesh <58168246+artinalbert@users.noreply.github.com>
2022-04-17 12:30:58 +0200

    loop example for dictionaries added.

---
+++ src/fibonacci/loops.py
@@ -0,0 +1,17 @@
+def main():
+    dic = {
+        1:'a', 2:'b', 3:'c' , 4:'d', 5:'e', -1:'f', 7:'f'
+        }
+    dic2 = {}
+    for i in dic.keys():
+        # i = i + 1
+        dic2[i+1] = dic[i]
+
+        # dic2[1] = i
+        # will make a new key if not already there (only if on left side of the assignment ioerator!)
+        # dic[-1] = i
+    print (dic)
+    print (dic2)
+
+if __name__== "__main__":
+    main() 
