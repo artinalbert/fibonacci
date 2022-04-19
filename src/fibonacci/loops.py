feca3985d8c67fbba8149d59d260c42141071070
Amirhossein Dabaghmanesh <58168246+artinalbert@users.noreply.github.com>
2022-04-19 08:14:05 +0200

    PEP warnings fixed

--- src/fibonacci/loops.py
+++ src/fibonacci/loops.py
@@ -1,6 +1,6 @@
 def main():
     dic = {
-        1:'a', 2:'b', 3:'c' , 4:'d', 5:'e', -1:'f', 7:'f'
+        1: 'a', 2: 'b', 3: 'c' , 4: 'd', 5: 'e', -1: 'f', 7: 'f'
         }
     dic2 = {}
     for i in dic.keys():
@@ -10,8 +10,8 @@ def main():
         # dic2[1] = i
         # will make a new key if not already there (only if on left side of the assignment ioeratorgit !)
         # dic[-1] = i
-    print (dic)
-    print (dic2)
+    print(dic)
+    print(dic2)
 
 if __name__== "__main__":
-    main() 
+    main()
